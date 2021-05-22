7a608547c236d8832724f2c2215917c774804520
thebesttv <taojuntian@outlook.com>
2021-05-22 19:55:26 +0800

    add 2021-05-22 & preview function

--- server.el
+++ server.el
@@ -159,3 +159,14 @@ string."
                          (format "%s\n\n%s" res src))
                        (extract-function-from-file file fun)))
           "\n#+END_SRC"))
+
+(defun eserver-blog-preview ()
+  (interactive)
+  (if (not (string-prefix-p eserver-blog (buffer-file-name)))
+      (message "This buffer is not under `eserver-blog'.")
+    (save-buffer)
+    (let ((url (format "http://localhost:8080/blog%s"
+                       (string-remove-prefix eserver-blog (buffer-file-name)))))
+      (eww-browse-url url))))
+
+(global-set-key (kbd "<f6>") 'eserver-blog-preview)
